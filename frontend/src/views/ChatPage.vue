e3c042fbc1ba6f82125b5ff1b06abdedc4e5d939
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2025-05-04 15:17:13 +0530

    made dockerfiles for deployment

--- frontend/src/views/ChatPage.vue
+++ frontend/src/views/ChatPage.vue
@@ -224,7 +224,7 @@ export default {
     connectToSocket() {
       supabase.auth.getSession().then(({ data }) => {
         const token = data.session?.access_token
-        this.socket = io(this.SOCKET_URL, { auth: { token }, extraHeaders: { Authorization: token ? `Bearer ${token}` : '' } })
+        this.socket = io(this.SOCKET_URL, { auth: { token },path: "/socket.io",transports: ["polling","websocket"],reconnectionAttempts: 5, extraHeaders: { Authorization: token ? `Bearer ${token}` : '' } })
         this.socket.on('connect', this.handleConnect)
         this.socket.on('connect_error', this.handleConnectionError)
         this.socket.on('disconnect', this.handleDisconnect)
