13b07896e8c7868c8d518b9673d8e1c424d83b24
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2025-05-14 13:25:17 +0530

    pinned model versions

--- application/ai/llm.py
+++ application/ai/llm.py
@@ -2,13 +2,13 @@ from langchain_openai import ChatOpenAI
 import os
 
 actor_llm = ChatOpenAI(
-    model_name="gpt-4.1",
-    temperature=0.5,
+    model_name="gpt-4.1-2025-04-14",
+    temperature=0.8,
     api_key=os.getenv('OPENAI_API_KEY')
 )
 
 director_llm = ChatOpenAI(
-    model_name="gpt-4.1-mini",
-    temperature=0.3,
+    model_name="gpt-4.1-mini-2025-04-14",
+    temperature=0.5,
     api_key=os.getenv('OPENAI_API_KEY')
 )
