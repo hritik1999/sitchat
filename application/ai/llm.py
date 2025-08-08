8627d882904671074e1002ce77d5bf7d02b8e1b5
hritik1999 <65231550+hritik1999@users.noreply.github.com>
2025-08-08 23:41:03 +0530

    updagraded to gpt-5

--- application/ai/llm.py
+++ application/ai/llm.py
@@ -2,13 +2,13 @@ from langchain_openai import ChatOpenAI
 import os
 
 actor_llm = ChatOpenAI(
-    model_name="gpt-4.1-2025-04-14",
+    model_name="gpt-5-2025-08-07",
     temperature=0.5,
     api_key=os.getenv('OPENAI_API_KEY')
 )
 
 director_llm = ChatOpenAI(
-    model_name="gpt-4.1-mini-2025-04-14",
+    model_name="gpt-5-mini",
     temperature=0.3,
     api_key=os.getenv('OPENAI_API_KEY')
 )
